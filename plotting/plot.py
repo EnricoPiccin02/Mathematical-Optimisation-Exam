db27af7f23df77776392fb453e583bf0db398977
EnricoPiccin02 <98701734+EnricoPiccin02@users.noreply.github.com>
2025-07-19 15:34:56 +0200

    Add files via upload

--- plotting/plot.py
+++ plotting/plot.py
@@ -62,13 +62,14 @@ def plot_runtime_boxplot(data, method_name, set_instance_name):
     plt.setp(legend.get_texts(), color='white')
     plt.setp(legend.get_title(), color='white')
 
+    # Axes border
     for spine in ax.spines.values():
         spine.set_edgecolor('white')
 
     plt.tight_layout()
     plt.show()
 
-def plot_matheuristic(df_mh, df_opt, set_instance_name):
+def plot_matheuristic_relative_difference(df_mh, df_opt, set_instance_name):
     df_mh = df_mh.copy()
     df_opt = df_opt.copy()
 
@@ -107,7 +108,7 @@ def plot_matheuristic(df_mh, df_opt, set_instance_name):
         x = group["d"].map(d_to_num).values
         y = group["delta_percent"].values
 
-        # Smooth curve if possible
+        # Smooth curve
         if len(x) >= 3:
             x_smooth = np.linspace(x.min(), x.max(), 300)
             spline = make_interp_spline(x, y, k=2)
@@ -143,6 +144,85 @@ def plot_matheuristic(df_mh, df_opt, set_instance_name):
     plt.tight_layout()
     plt.show()
 
+def plot_matheuristic_runtime_bars(data):
+    df_mh = data.copy()
+    plt.rcParams.update({'axes.facecolor': 'white', 'figure.facecolor': 'white'})
+
+    ### First plot: Node series
+
+    # Average time grouped by arc density and n
+    df_node = df_mh.groupby(['d', 'n'])['TotalTime'].mean().reset_index()
+    df_node['n_str'] = df_node['n'].apply(lambda x: f"N{x}")
+    df_node = df_node.sort_values(['d', 'n'])
+
+    fig1, ax1 = plt.subplots(figsize=(10, 6))
+    fig1.patch.set_facecolor('#050505')
+    ax1.set_facecolor('#050505')
+
+    custom_palette = sns.color_palette("bright")
+
+    sns.barplot(
+        data=df_node,
+        x='d',
+        y='TotalTime',
+        hue='n_str',
+        palette=custom_palette,
+        ax=ax1
+    )
+
+    ax1.set_title("Node series", fontsize=13, color='white')
+    ax1.set_xlabel("Arc density", color='white')
+    ax1.set_ylabel("Time (s)", color='white')
+    ax1.tick_params(colors='white')
+    legend = ax1.legend(title='', loc='upper left', frameon=False)
+    plt.setp(legend.get_texts(), color='white')
+    plt.setp(legend.get_title(), color='white')
+    for spine in ax1.spines.values():
+        spine.set_edgecolor('white')
+
+    plt.tight_layout()
+    plt.show()
+
+    ### Second plot: Conflict series
+
+    def label_r(r):
+        if r == 1:
+            return 'CD1'
+        elif r == 2:
+            return 'CD2'
+        else:
+            return 'CD3'
+    df_mh = data[data['n'].isin([300, 400, 500])].copy()
+    df_mh['r_value'] = df_mh['instance_id'].str.extract(r'R(\d*\.?\d+)').astype(float)
+    df_mh['r'] = df_mh['r_value'].apply(label_r)
+    df_conflict = df_mh.groupby(['d', 'r'])['TotalTime'].mean().reset_index()
+    df_conflict = df_conflict.sort_values(['d', 'r'])
+
+    fig2, ax2 = plt.subplots(figsize=(10, 6))
+    fig2.patch.set_facecolor('#050505')
+    ax2.set_facecolor('#050505')
+
+    sns.barplot(
+        data=df_conflict,
+        x='d',
+        y='TotalTime',
+        hue='r',
+        palette=custom_palette,
+        ax=ax2
+    )
+
+    ax2.set_title("Conflict series", fontsize=13, color='white')
+    ax2.set_xlabel("Arc density", color='white')
+    ax2.set_ylabel("Time (s)", color='white')
+    ax2.tick_params(colors='white')
+    legend = ax2.legend(title='', loc='upper left', frameon=False)
+    plt.setp(legend.get_texts(), color='white')
+    plt.setp(legend.get_title(), color='white')
+    for spine in ax2.spines.values():
+        spine.set_edgecolor('white')
+
+    plt.tight_layout()
+    plt.show()
 
 # ------------------------------------------------
 # Plotting ILP, MILP, Matheuristic (RAND and SWN)
@@ -153,14 +233,14 @@ if __name__ == "__main__":
     sns.set_theme(style="whitegrid")
 
     # Read the RND_experimental_results.csv file
-    rnd_df = pd.read_csv("RND_experimental_results.csv")
+    rnd_df = pd.read_csv("RND_experimental_results_modified.csv")
     plot_runtime_boxplot(rnd_df[rnd_df["method"] == "ILP"], "ILP", "RAND") # Plot for ILP (RAND)
     plot_runtime_boxplot(rnd_df[rnd_df["method"] == "MILP"], "MILP", "RAND") # Plot for MILP (RAND)
-    plot_matheuristic(rnd_df[rnd_df["method"] == "Matheuristic"], rnd_df[rnd_df["method"] == "MILP"], "RAND") # Plot for Matheuristic (RAND)
-
+    plot_matheuristic_relative_difference(rnd_df[rnd_df["method"] == "Matheuristic"], rnd_df[rnd_df["method"] == "MILP"], "RAND") # Plot for Matheuristic (RAND)
+    plot_matheuristic_runtime_bars(rnd_df[rnd_df["method"] == "Matheuristic"])
 
     # Read the SWN_experimental_results.csv file
     swn_df = pd.read_csv("SWN_experimental_results.csv")
     plot_runtime_boxplot(swn_df[swn_df["method"] == "ILP"], "ILP", "SWN") # Plot for ILP (SWN)
     plot_runtime_boxplot(swn_df[swn_df["method"] == "MILP"], "MILP", "SWN") # Plot for MILP (SWN)
-    plot_matheuristic(swn_df[swn_df["method"] == "Matheuristic"], swn_df[swn_df["method"] == "MILP"], "SWN") # Plot for Matheuristic (SWN)
+    plot_matheuristic_relative_difference(swn_df[swn_df["method"] == "Matheuristic"], swn_df[swn_df["method"] == "MILP"], "SWN") # Plot for Matheuristic (SWN)
